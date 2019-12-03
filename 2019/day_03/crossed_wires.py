ed22e11558dfa769ab61e49a83d06506434860ec
David Colton <davidcolton@gmail.com>
2019-12-03 18:56:47 +0000

    Refactored Part 01 to be a class, easier to extend

--- 2019/day_03/crossed_wires.py
+++ 2019/day_03/crossed_wires.py
@@ -3,77 +3,76 @@ from collections import namedtuple
 PathCoords = namedtuple("PathCoords", "x y dist")
 
 
-def _get_max_direction(wire: list) -> int:
-    max = 0
-    for dir in wire:
-        dist = int(dir[1:])
-        max = dist if dist > max else max
-    return max
+class WirePath:
+    def __init__(self, path: list):
+        self.root = PathCoords(0, 0, 0)
+        self.current = PathCoords(0, 0, 0)
+        self.path = path
+        self.steps = 0
 
+        self.wire_path_coords = set()
+        self.steps_taken = dict()
 
-def _move_up(path: dict, coords: PathCoords, length: str):
-    start_x = coords.x
-    start_y = coords.y
-    dist = int(length[1:])
-    for n in range(1, dist + 1):
-        new_coords = PathCoords(start_x, start_y + n, abs(start_x) + abs(start_y + n))
-        path.add(new_coords)
-    return path, new_coords
+    def move_up(self, length: str):
+        x = self.current.x
+        y = self.current.y
+        moves = int(length[1:])
+        for n in range(1, moves + 1):
+            dist = abs(x) + abs(y + n)
+            new_coords = PathCoords(x, y + n, dist)
+            self.wire_path_coords.add(new_coords)
+        self.current = new_coords
 
+    def move_down(self, length: str):
+        x = self.current.x
+        y = self.current.y
+        moves = int(length[1:])
+        for n in range(1, moves + 1):
+            dist = abs(x) + abs(y - n)
+            new_coords = PathCoords(x, y - n, dist)
+            self.wire_path_coords.add(new_coords)
+        self.current = new_coords
 
-def _move_down(path: set, coords: PathCoords, length: str):
-    start_x = coords.x
-    start_y = coords.y
-    dist = int(length[1:])
-    for n in range(1, dist + 1):
-        new_coords = PathCoords(start_x, start_y - n, abs(start_x) + abs(start_y - n))
-        path.add(new_coords)
-    return path, new_coords
+    def move_right(self, length: str):
+        x = self.current.x
+        y = self.current.y
+        moves = int(length[1:])
+        for n in range(1, moves + 1):
+            dist = abs(x + n) + abs(y)
+            new_coords = PathCoords(x + n, y, dist)
+            self.wire_path_coords.add(new_coords)
+        self.current = new_coords
 
+    def move_left(self, length: str):
+        x = self.current.x
+        y = self.current.y
+        moves = int(length[1:])
+        for n in range(1, moves + 1):
+            dist = abs(x - n) + abs(y)
+            new_coords = PathCoords(x - n, y, dist)
+            self.wire_path_coords.add(new_coords)
+        self.current = new_coords
 
-def _move_right(path: set, coords: PathCoords, length: str):
-    start_x = coords.x
-    start_y = coords.y
-    dist = int(length[1:])
-    for n in range(1, dist + 1):
-        new_coords = PathCoords(start_x + n, start_y, abs(start_x + n) + abs(start_y))
-        path.add(new_coords)
-    return path, new_coords
-
-
-def _move_left(path: set, coords: PathCoords, length: str):
-    start_x = coords.x
-    start_y = coords.y
-    dist = int(length[1:])
-    for n in range(1, dist + 1):
-        new_coords = PathCoords(start_x - n, start_y, abs(start_x - n) + abs(start_y))
-        path.add(new_coords)
-    return path, new_coords
-
-
-def _follow_path(coords: set, root: PathCoords, wire: list) -> set:
-    steps_taken = 0
-    for instruction in wire:
-        if instruction.startswith("U"):
-            coords, root = _move_up(coords, root, instruction)
-        elif instruction.startswith("D"):
-            coords, root = _move_down(coords, root, instruction)
-        elif instruction.startswith("L"):
-            coords, root = _move_left(coords, root, instruction)
-        else:
-            coords, root = _move_right(coords, root, instruction)
-    return coords
+    def follow_path(self) -> set:
+        for instruction in self.path:
+            if instruction.startswith("U"):
+                self.move_up(instruction)
+            elif instruction.startswith("D"):
+                self.move_down(instruction)
+            elif instruction.startswith("L"):
+                self.move_left(instruction)
+            else:
+                self.move_right(instruction)
+        return self.wire_path_coords
 
 
 def crossed_wires(wire_01: list, wire_02: list) -> int:
 
-    root = PathCoords(0, 0, 0)
+    coords_wire_01 = WirePath(wire_01)
+    coords_wire_02 = WirePath(wire_02)
 
-    coords_wire_01 = set()
-    coords_wire_02 = set()
-
-    wire_01_coords = _follow_path(coords_wire_01, root, wire_01)
-    wire_02_coords = _follow_path(coords_wire_02, root, wire_02)
+    wire_01_coords = coords_wire_01.follow_path()
+    wire_02_coords = coords_wire_02.follow_path()
 
     intersetion_points = wire_01_coords.intersection(wire_02_coords)
 
@@ -88,4 +87,4 @@ with open("input.txt", "r") as f:
 wire_01 = lines[0].split(",")
 wire_02 = lines[1].split(",")
 
-print((crossed_wires(wire_01, wire_02)))
+print((crossed_wires(["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"])))
