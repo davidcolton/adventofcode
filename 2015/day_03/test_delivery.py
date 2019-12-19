4d456ab3d2122b92e44b0554865569e7fdc56bee
David Colton <davidcolton@gmail.com>
2019-12-19 10:51:08 +0000

    Working solution for AventofCode 2015 Day 03

---
+++ 2015/day_03/test_delivery.py
@@ -0,0 +1,18 @@
+import pytest
+from delivery import Point, DeliveryRoute
+
+
+@pytest.mark.parametrize("arg, expected", [(">", 2), ("^>v<", 4), ("^v^v^v^v^v", 2)])
+def test_houses_delivered(arg, expected):
+    route = DeliveryRoute(arg)
+    route.follow_route()
+    assert route.houses_visited == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected", [(["^v", 2], 3), (["^>v<", 2], 3), (["^v^v^v^v^v", 2], 11)]
+)
+def test_houses_delivered_two_santas(arg, expected):
+    route = DeliveryRoute(arg[0], arg[1])
+    route.follow_route()
+    assert route.houses_visited == expected
