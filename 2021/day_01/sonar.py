34b7786b86b362cc09fe9de17b191980ba4c0a89
davidcolton <davidcolton@gmail.com>
2021-12-01 12:12:34 +0000

    Day 01, Part 02 Completed

--- 2021/day_01/sonar.py
+++ 2021/day_01/sonar.py
@@ -1,7 +1,7 @@
-def examine_sonar(input):
+def examine_sonar(input, window):
     increases = 0
-    for position in range(len(input) - 1):
-        if input[position + 1] > input[position]:
+    for position in range(len(input) - window):
+        if sum(input[position + 1:position+window+1]) > sum(input[position:position+window]):
             increases += 1
 
     print(f"Solution Found: {increases}")
@@ -18,5 +18,7 @@ if __name__ == "__main__":
     with open("input") as f:
         values = [int(line.strip()) for line in f]
 
-    answer = examine_sonar(values)
+    window = 3
+
+    answer = examine_sonar(values, window)
 
--- 2021/day_01/test_sonar.py
+++ 2021/day_01/test_sonar.py
@@ -3,12 +3,15 @@ from sonar import examine_sonar
 
 
 @pytest.mark.parametrize(
-    "input, expected",
+    "input, window, expected",
     [
-        ([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7),
-        ([199, 200, 208, 210, 240, 269], 5),
-        ([199, 100], 0),
+        ([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 1, 7),
+        ([199, 200, 208, 210, 240, 269], 1, 5),
+        ([199, 100, 95, 80], 1, 0),
+        ([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 3, 5),
+        ([199, 200, 208, 210, 240, 269], 3, 3),
+        ([199, 100, 95, 80], 3, 0),
     ],
 )
-def test_examine_sonar(input, expected):
-    assert examine_sonar(input) == expected
+def test_examine_sonar(input, window, expected):
+    assert examine_sonar(input, window) == expected
