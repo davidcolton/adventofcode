a9c168cb4ce3e0993880569b65fb50309a2bbfd8
davidcolton <davidcolton@gmail.com>
2021-12-03 23:13:35 +0000

    Day 03, Part 02 Complete

--- 2021/day_03/binary.py
+++ 2021/day_03/binary.py
@@ -6,6 +6,34 @@ def get_power(df):
     epsilon = int(''.join(map(str, list(val_counts.idxmin()))), 2)
     return gamma * epsilon
 
+def get_oxygen(df):
+    for col in range(1, df.shape[1] + 1):
+        max_val = df[col].value_counts().idxmax()
+        min_val = df[col].value_counts().idxmin()
+        if df[col].value_counts()[0] == df[col].value_counts()[1]:
+            max_val = 1
+        df = df[df[col] == max_val]
+        if df.shape[0] == 1:
+            break
+    return int(''.join(map(str, df.values.flatten().tolist())), 2)
+
+def get_carbon(df):
+    for col in range(1, df.shape[1] + 1):
+        max_val = df[col].value_counts().idxmax()
+        min_val = df[col].value_counts().idxmin()
+        if df[col].value_counts()[0] == df[col].value_counts()[1]:
+            min_val = 0
+        df = df[df[col] == min_val]
+        if df.shape[0] == 1:
+            break
+    return int(''.join(map(str, df.values.flatten().tolist())), 2)
+
+def get_life_support(df):
+
+    oxygen = get_oxygen(df.copy())
+    carbon = get_carbon(df.copy())
+    return oxygen * carbon
+
 if __name__ == "__main__":
 
     # Read in the input ... always called `input`
@@ -18,3 +46,4 @@ if __name__ == "__main__":
     df = df.astype(int)
 
     print(f'Power Consumption is: {get_power(df)}')
+    print(f'Life Support Rating is: {get_life_support(df)}')
--- 2021/day_03/test_binary.py
+++ 2021/day_03/test_binary.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 from binary import get_power
+from binary import get_life_support
 
 data = [
     [0,0,1,0,0],
@@ -17,7 +18,7 @@ data = [
     [0,1,0,1,0],
 ]
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns=[1,2,3,4,5])
 
 @pytest.mark.parametrize(
     "input, expected",
@@ -27,3 +28,12 @@ df = pd.DataFrame(data)
 )
 def test_get_power(input, expected):
     assert get_power(input) == expected
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (df, 230),
+    ],
+)
+def test_get_life_support(input, expected):
+    assert get_life_support(input) == expected
